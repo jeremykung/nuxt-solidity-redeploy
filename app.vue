324e7bee57910f4104fcdc70e46b1e4c012b1bbc
jeremykung <jeremykung@gmail.com>
2023-11-07 22:41:20 +0200

    integrated web3

--- app.vue
+++ app.vue
@@ -7,8 +7,21 @@
 </template>
 
 <script setup>
+import Web3 from "web3"
 import contractABI from "/abis/abi.json"
-console.log("abi:", contractABI)
 
 const contractAddress = "0x2f7C319eDf93364976E548E659f3400d5A511Ff0"
+const web3 = ref(null)
+
+onMounted(async () => {
+  if (process.client && window.ethereum) {
+    web3.value = new Web3(window.ethereum)
+    console.log("web3:", web3.value)
+  } else {
+    console.log(
+      "Non-Ethereum browser detected. You should consider trying MetaMask!"
+    )
+  }
+})
+console.log("abi:", contractABI)
 </script>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,9 @@
   <nav class="container py-4 mb-6 flex justify-between items-center">
     <div class="text-xl text-slate-500">Nuxt x Solidity</div>
     <div class="flex items-center gap-2">
-      <UButton color="gray" variant="solid">Connect</UButton>
+      <UButton @click="connectWallet" color="gray" variant="solid"
+        >Connect</UButton
+      >
       <ClientOnly>
         <UButton
           :icon="
@@ -19,6 +21,18 @@
 </template>
 
 <script setup>
+async function connectWallet() {
+  const accounts = await window.ethereum
+    .request({ method: "eth_requestAccounts" })
+    .catch((err) => {
+      if (err.code === 4001) {
+        console.log("Please connect to MetaMask.")
+      } else {
+        console.error(err)
+      }
+    })
+}
+
 const colorMode = useColorMode()
 
 const isDark = computed({
