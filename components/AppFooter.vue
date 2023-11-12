5c6f5ab60592a764ce84ed32bb8cde41c637aa94
jeremykung <jeremykung@gmail.com>
2023-11-13 00:25:47 +0200

    created footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <UDivider
+    label=""
+    class="mt-8"
+    :ui="{ border: { size: { horizontal: 'border-t-2' } } }"
+  />
+
+  <div class="text-right font-thin text-sm italic py-4">
+    Created by <span class="text-primary font-normal">Jeremy Kung</span>
+  </div>
+</template>
+
+<style scoped>
+/* .footer-container {
+  border-top: 1px solid gray;
+} */
+</style>
