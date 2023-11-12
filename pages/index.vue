55dce58d32cd1e34bd9806bd2d7282c12f78e035
jeremykung <jeremykung@gmail.com>
2023-11-13 00:27:13 +0200

    added quick start

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
+  <!-- Quick Start -->
+  <div class="mb-8">
+    <h1 class="text-2xl">Quick Start Guide</h1>
+    <div class="font-thin text-gray-400">
+      <p>1. Install Metamask & switch to Sepolia Test Network</p>
+      <p>
+        2. Get some test
+        <a
+          class="text-primary"
+          target="_blank"
+          href="https://sepoliafaucet.com/"
+          >Sepolia Eth</a
+        >
+      </p>
+      <p>3. Create a new post that will live on-chain</p>
+    </div>
+  </div>
+
+  <!-- Posts -->
   <div class="md:flex md:flex-row w-full md:justify-between flex-col">
     <CreatePostForm class="md:w-2/5 w-full" />
     <DisplayPosts class="md:w-2/5 md:mt-0 mt-8" />
-    <div></div>
   </div>
 </template>
