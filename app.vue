5502b2ba5ad0aa5e10fa822e666ae4b24bc4c411
jeremykung <jeremykung@gmail.com>
2023-11-26 13:05:25 +0200

    working on walletConnect for mobile link

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <UContainer>
-    <p>Test 1</p>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -10,35 +9,99 @@
 <script setup>
 import Web3 from "web3"
 import blogContractABI from "/abis/blogAbi.json"
-
-// This function detects most providers injected at window.ethereum.
 import detectEthereumProvider from "@metamask/detect-provider"
 
 const web3Store = useWeb3Store()
 
-onMounted(async () => {
-  console.log("test 1")
-  // This returns the provider, or null if it wasn't detected.
+// Wallet Connect
+import { Core } from "@walletconnect/core"
+import { Web3Wallet } from "@walletconnect/web3wallet"
+import { buildApprovedNamespaces, getSdkError } from "@walletconnect/utils"
+
+const runtimeConfig = useRuntimeConfig() // dotenv setup via nuxt.config
+
+const core = new Core({
+  projectId: runtimeConfig.public.PROJECT_ID,
+})
+
+console.log("core:", core)
+
+const web3wallet = await Web3Wallet.init({
+  core, // <- pass the shared `core` instance
+  metadata: {
+    name: "Nuxt x Solidity",
+    description: "Demo Client as Wallet/Peer",
+    url: "www.walletconnect.com",
+    icons: [],
+  },
+})
+
+async function onSessionProposal({ id, params }) {
   try {
-    const provider = await detectEthereumProvider()
-    console.log("provider:", provider)
+    // ------- namespaces builder util ------------ //
+    const approvedNamespaces = buildApprovedNamespaces({
+      proposal: params,
+      supportedNamespaces: {
+        eip155: {
+          chains: ["eip155:1", "eip155:137"],
+          methods: ["eth_sendTransaction", "personal_sign"],
+          events: ["accountsChanged", "chainChanged"],
+          accounts: [
+            "eip155:1:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb",
+            "eip155:137:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb",
+          ],
+        },
+      },
+    })
+    // ------- end namespaces builder util ------------ //
 
-    if (provider) {
-      // From now on, this should always be true:
-      // provider === window.ethereum
-      startApp(provider) // initialize your app
-    } else {
-      console.log("Please install MetaMask!")
-    }
+    const session = await web3wallet.approveSession({
+      id,
+      namespaces: approvedNamespaces,
+    })
   } catch (error) {
-    console.log("error detecting provider:", error)
+    // use the error.message to show toast/info-box letting the user know that the connection attempt was unsuccessful
+    console.log("error:", error)
+    await web3wallet.rejectSession({
+      id: proposal.id,
+      reason: getSdkError("USER_REJECTED"),
+    })
   }
+}
 
+web3wallet.on("session_proposal", onSessionProposal)
+
+console.log("web3wallet", web3wallet)
+
+onMounted(async () => {
   // Connect web3 Provider
   if (process.client && window.ethereum) {
     try {
+      // Detect User Network
+
+      const chainId = await window.ethereum.request({ method: "eth_chainId" })
+      console.log("chainId", chainId)
+
+      window.ethereum.on("chainChanged", handleChainChanged)
+
+      function handleChainChanged(chainId) {
+        // We recommend reloading the page, unless you must do otherwise.
+        window.location.reload()
+      }
+
+      // Create web3 Instance
       const web3 = await new Web3(window.ethereum)
       await web3Store.setWeb3Instance(web3)
+      console.log("web3:", web3Store.web3)
+
+      // Link contract ABI
+      let blogContract = await new web3Store.web3.eth.Contract(
+        blogContractABI,
+        web3Store.blogContractAddress
+      )
+      await web3Store.setBlogContract(blogContract)
+
+      console.log("blog contract:", web3Store.blogContract)
     } catch (error) {
       console.log("error finding window.ethereum:", error)
     }
@@ -47,16 +110,7 @@ onMounted(async () => {
       "Non-Ethereum browser detected. You should consider trying MetaMask!"
     )
   }
-
-  console.log("web3:", web3Store.web3)
-
-  let blogContract = await new web3Store.web3.eth.Contract(
-    blogContractABI,
-    web3Store.blogContractAddress
-  )
-  await web3Store.setBlogContract(blogContract)
-
-  console.log("blog contract:", web3Store.blogContract)
 })
+
 // console.log("abi:", contractABI)
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // server: {
+  //   host: "0.0.0.0", // default: localhost
+  // },
+  runtimeConfig: {
+    BLOG_CONTRACT: "0x2f7C319eDf93364976E548E659f3400d5A511Ff0",
+    public: {
+      PROJECT_ID: "bf01010b0dbe1ccc528498b6e8dcd161",
+      TEST_VARIABLE: "Hello Dotenv",
+    },
+  },
   devtools: { enabled: true },
   modules: [
     "@nuxt/ui",
